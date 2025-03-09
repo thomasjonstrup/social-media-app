e6b85790f1d3a9d1d7e7f2dfdd1b4d61d47c67d7
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2025-03-09 21:52:36 +0100

    fix: issue with unused inport

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { PostList } from "@/components/PostList";
 import { postsQueryOptions } from "@/postsQueryOptions";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { Link, createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 const Home = () => {
 	const { data, isLoading } = useSuspenseQuery(postsQueryOptions);
